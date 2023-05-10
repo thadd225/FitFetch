9920cce096825c8c0c5b29d8553af66be820a3f9
Thad White <thadd225@gmail.com>
2023-05-09 20:57:52 -0400

    created post handler in server.js as well as handle click function in exercises.js. Also installed cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,39 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+//needed cors!! (something security relatedß to do with frontend url being different than server)
+const cors = require('cors');
 
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
+//normal endpoint handling
 app.get('/', (req, res) => {
   console.log('hello world');
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+//request handler for posting exercises when exercise button is clicked
+app.post('/add', (req, res) => {
+  console.log('post request hit serverside');
+  console.log('req.body', req.body);
+  res.status(200).json({ message: 'Exercise added successfully ' });
+});
+
+// catch-all route handler for any requests to an unknown route
+app.use('*', (req, res) => res.sendStatus(404));
+
+//global handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => {
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -4,7 +4,16 @@ const Exercises = (props) => {
   //alert user to confirm selection when clicking exercise button?
 
   //when user clicks exercise button, send post request to /add (using fetch)
-  const handleExerciseClick = (exercise) => {};
+  const handleExerciseClick = async (exercise) => {
+    console.log('exercise: ', typeof exercise);
+    fetch(`http://localhost:3000/add`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ exercise }),
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error('Error: ', error));
+  };
   //MUST HAVE 2 RETURN STATEMENTS WHEN USING MAP TO RENDER
   //one return for entire div, one for each indivual li
   return (
@@ -13,7 +22,12 @@ const Exercises = (props) => {
         {props.name.map((data, index) => {
           return (
             <li className="exercises" key={index}>
-              <button className="button-59">{data.name}</button>
+              <button
+                className="button-59"
+                onClick={() => handleExerciseClick(data.name)}
+              >
+                {data.name}
+              </button>
             </li>
           );
         })}
